fec1fc0f2dc3f5da85d239db59e818374290d12b
Kentaro Matsumae <kentaro.matsumae@gmail.com>
2014-05-04 20:23:45 +0900

    fix

---
+++ Assets/Ground.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ground : MonoBehaviour {
+
+	public GameObject groundControl;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//if out of camera, recycle this ground.
+		Vector3 pos = Camera.main.WorldToViewportPoint(this.transform.position);	
+		if (pos.z < 0) {
+			GroundControl con = groundControl.GetComponent<GroundControl>();
+			con.recycle(this.gameObject);
+		}
+	}
+
+}
--- Assets/GroundControl.cs
+++ Assets/GroundControl.cs
@@ -37,66 +37,27 @@ public class GroundControl : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		Vector2 userPos = new Vector2(user.transform.position.x,
-		                              user.transform.position.z);
-
-		GameObject ground = this.grounds.First.Value;
-
-		//z-y check
-		Rect groundRect;
-		float angle = ground.transform.rotation.eulerAngles.y;
-		if (angle == 0 || angle == 180) {
-			float z = ground.transform.position.z;
-			float x = ground.transform.position.x;
-			float width = ground.transform.localScale.x;
-			float height = ground.transform.localScale.z; //user and ground.edge offset
-			groundRect = new Rect(x - (width / 2),
-			                      z + (height / 2),
-			                      width,
-			                      height);
-		} else {
-			float z = ground.transform.position.z;
-			float x = ground.transform.position.x;
-			float width = ground.transform.localScale.z;
-			float height = ground.transform.localScale.x; //user and ground.edge offset
-			groundRect = new Rect(x - (width / 2),
-			                      z + (height / 2),
-			                      width,
-			                      height);
-		}
-
-
-		//Debug.Log(string.Format("{0} {1} {2}", ground.name, groundRect, userPos));
-
-		//on ground ?
-		if (groundRect.Contains(userPos)) {
-			Debug.Log(string.Format("out! {0}", ground.name));
-			Debug.Log(string.Format("{0} {1} {2} y:{3}", ground.name, groundRect, userPos, ground.transform.rotation.eulerAngles));
-			this.recycleGround();
-		}
 	}
 
-	private void recycleGround()
-	{
+	public void recycle(GameObject targetGround) {
+		Debug.Log(string.Format("recycle:{0}", targetGround));
+
 		GameObject tailGround = this.grounds.Last.Value;
 
-		LinkedListNode<GameObject> targetNode = this.grounds.First;
-		this.grounds.RemoveFirst();
-		this.grounds.AddLast(targetNode);
+		this.grounds.Remove(targetGround);
+		this.grounds.AddLast(targetGround);
 
-		GameObject targetGround = targetNode.Value;
 		targetGround.transform.rotation = tailGround.transform.rotation;
-
-		// add straight
+		
+		// add straigh
 		Vector3 pos = tailGround.transform.position;
 		pos.z += tailGround.transform.localScale.z;
 		targetGround.transform.position = pos;
-
+		
 		Vector3 rotatePoint = new Vector3(pos.x,
 		                                  pos.y,
 		                                  pos.z - (targetGround.transform.localScale.z / 2) + (targetGround.transform.localScale.x / 2));
 		targetGround.transform.RotateAround(rotatePoint, Vector3.up, 90);
-
-
 	}
+
 }
