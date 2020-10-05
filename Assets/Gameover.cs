bc7fc2695787ddd62f3dbcdd007a809d589d6c4e
Udhay <udhaybrahmi@gmail.com>
2020-10-05 11:13:13 +0530

    Ground texture changed , Game music added , Music on die added , Restart game button added with hover feature ,Quit game button added with hover feature.

---
+++ Assets/Gameover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Gameover : MonoBehaviour
+{
+    public GameObject gameovertune , gametune;
+    void Start() {
+        gameovertune.SetActive(true);
+        gametune.SetActive(false);
+    }
+    public void restart() {
+        SceneManager.LoadScene(0);
+    }
+    public void quit() {
+        Application.Quit();
+    }
+}
